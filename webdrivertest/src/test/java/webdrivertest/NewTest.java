cdf754cbc23a9d574d1920d0420cca4bd867e8e7
Dell <Dell@192.168.1.6>
2017-06-15 18:41:47 +0530

    Committing initial version

---
+++ webdrivertest/src/test/java/webdrivertest/NewTest.java
@@ -0,0 +1,29 @@
+package webdrivertest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+
+public class NewTest {
+	WebDriver driver;
+  @Test
+  public void testeasy() {
+	  driver.get("http://demo.guru99.com/selenium/guru99home/");
+	  String title=driver.getTitle();
+	  //Assert.assertTrue(title.contains("Demo Guru99 page"));
+	    }
+  @BeforeTest
+  public void beforetest(){
+	  System.setProperty("webdriver.chrome.driver", "D://Selenium/ChromeDriver/chromedriver.exe");
+	  driver=new ChromeDriver();
+  }
+  @AfterTest
+  public void aftertest(){
+	  driver.quit();
+  }
+}
